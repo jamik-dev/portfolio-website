e15c5fd061485e258574220f13b1d195884ca66c
Jamshid <jamik.oik.group@gmail.com>
2022-03-10 21:59:34 +0500

    without hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,41 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'DefaultLayout',
-    component: import('../layouts/DefaultLayout.vue'),
+    component: import ('../layouts/DefaultLayout.vue'),
     redirect: '/home',
-    children: [
-      {
-        path: 'home',
-        name: 'Home',
-        component: import('../components/Home.vue'),
-      },
-      {
-        path: 'about',
-        name: 'About',
-        component: import('../components/About.vue'),
-      },
-      {
-        path: 'services',
-        name: 'Services',
-        component: import('../components/Services.vue'),
-      },
-      {
-        path: 'portfolio',
-        name: 'Portfolio',
-        component: import('../components/Portfolio.vue'),
-      },
-      {
-        path: 'clients',
-        name: 'Clients',
-        component: import('../components/Clients.vue'),
-      }
+    children: [{
+            path: 'home',
+            name: 'Home',
+            component: import ('../components/Home.vue'),
+        },
+        {
+            path: 'about',
+            name: 'About',
+            component: import ('../components/About.vue'),
+        },
+        {
+            path: 'services',
+            name: 'Services',
+            component: import ('../components/Services.vue'),
+        },
+        {
+            path: 'portfolio',
+            name: 'Portfolio',
+            component: import ('../components/Portfolio.vue'),
+        },
+        {
+            path: 'clients',
+            name: 'Clients',
+            component: import ('../components/Clients.vue'),
+        }
     ]
-  }
-]
+}]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
 export default router
